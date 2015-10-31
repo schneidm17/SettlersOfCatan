6620f00cfcfe6648daaf39ead319c1ca526b15ee
schneidm17 <schneidm17@up.edu>
2015-10-31 14:06:44 -0700

    Refactored Pig Lab and replaced all "Pig" files with "Catan" files

---
+++ app/src/main/java/edu/up/cs301/catan/CatanComputerPlayer.java
@@ -0,0 +1,32 @@
+package edu.up.cs301.catan;
+
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.infoMsg.GameInfo;
+
+/**
+ * An AI for Catan
+ *
+ * @author Andrew M. Nuxoll
+ * @version August 2015
+ */
+public class CatanComputerPlayer extends GameComputerPlayer {
+
+    /**
+     * ctor does nothing extra
+     */
+    public CatanComputerPlayer(String name) {
+        super(name);
+    }
+
+    /**
+     * callback method--game's state has changed
+     *
+     * @param info
+     * 		the information (presumably containing the game's state)
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+        // TODO  You will implement this method
+    }//receiveInfo
+
+}
---
+++ app/src/main/java/edu/up/cs301/catan/CatanMainActivity.java
@@ -0,0 +1,69 @@
+package edu.up.cs301.catan;
+
+import java.util.ArrayList;
+
+import edu.up.cs301.game.GameMainActivity;
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.LocalGame;
+import edu.up.cs301.game.config.GameConfig;
+import edu.up.cs301.game.config.GamePlayerType;
+import edu.up.cs301.catan.CatanComputerPlayer;
+import edu.up.cs301.catan.CatanHumanPlayer;
+import edu.up.cs301.catan.CatanLocalGame;
+
+/**
+ * this is the primary activity for Catan
+ *
+ * @author Andrew M. Nuxoll
+ * @version August 2015
+ */
+public class CatanMainActivity extends GameMainActivity {
+
+    // the port number that this game will use when playing over the network
+    private static final int PORT_NUMBER = 2278;
+
+    /**
+     * Create the default configuration for this game:
+     * - one human player vs. one computer player
+     * - minimum of 1 player, maximum of 2
+     *
+     * @return
+     * 		the new configuration object, representing the default configuration
+     */
+    @Override
+    public GameConfig createDefaultConfig() {
+
+        // Define the allowed player types
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+
+        // Catan has two player types:  human and computer
+        playerTypes.add(new GamePlayerType("Local Human Player") {
+            public GamePlayer createPlayer(String name) {
+                return new CatanHumanPlayer(name);
+            }});
+        playerTypes.add(new GamePlayerType("Computer Player") {
+            public GamePlayer createPlayer(String name) {
+                return new CatanComputerPlayer(name);
+            }});
+
+        // Create a game configuration class for Counter:
+        GameConfig defaultConfig = new GameConfig(playerTypes, 1, 2, "Catan", PORT_NUMBER);
+        defaultConfig.addPlayer("Human", 0); // player 1: a human player
+        defaultConfig.addPlayer("Computer", 1); // player 2: a computer player
+        defaultConfig.setRemoteData("Remote Player", "", 0);
+
+        return defaultConfig;
+    }//createDefaultConfig
+
+    /**
+     * create a local game
+     *
+     * @return
+     * 		the local game, a counter game
+     */
+    @Override
+    public LocalGame createLocalGame() {
+        return new CatanLocalGame();
+    }
+
+}
